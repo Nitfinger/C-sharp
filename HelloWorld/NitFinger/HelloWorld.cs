199c4120ef3c5705510167219164c7c6604db860
Nitfinger <34273894+Nitfinger@users.noreply.github.com>
2017-12-05 23:55:46 +1030

    Add files via upload

---
+++ HelloWorld/NitFinger/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NitFinger
+{
+    class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
